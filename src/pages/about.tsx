4d25cd7e853ba21b97f49e0f368e340aa9217e5a
Rodrigo Oliveira <rodrigocode4@gmail.com>
2020-11-05 11:31:06 -0300

    config typescript in nextjs

---
+++ src/pages/about.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head"
+import Link from 'next/link'
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <div>
+          <h1>
+            Sobre
+                    </h1>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </div>
+      </body>
+
+    </>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          React Avançado
+        </h1>
+        <Link href="/about">
+          <a>Sobre</a>
+        </Link>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
