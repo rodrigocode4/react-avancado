1f7cfa0521cb80c0152b23157bd267729bf92b9d
Rodrigo Oliveira <rodrigocode4@gmail.com>
2020-12-03 22:41:49 -0300

    replace addon-knobs to addon-essencials

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,6 @@ module.exports = {
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   'addons': [
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
   ]
 }
