9eada37efdd65174b1cd5956c8889d3000fc50c3
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-02-25 22:11:57 -0300

    migrate to stobook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,13 @@
 module.exports = {
-  'stories': [
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  'addons': [
-    '@storybook/addon-essentials',
-  ]
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  babel: async (options) => {
+    return ({
+    ...options,
+    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
+  })},
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
